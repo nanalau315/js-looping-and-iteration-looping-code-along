b30177d91ae52916968c6b8ec590ac880ed1c8c3
Nana Lau <nanalau315@hotmail.com>
2021-01-18 18:44:22 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(array, word){
+    let newArray = []
+    for (let i = 0; i < array.length; i++){
+        newArray.push(`Thank you, ${array[i]}, for the wonderful ${word} gift!`)
+    }
+    return newArray
+}
+
+function countDown(number){
+    for (let i = number; i > -1; i--){
+        console.log(i)
+    }
+}
